fbd14773692d63836d2e25aeb857391b6c4f2009
Reverend Philip <me@philiprehberger.com>
2026-03-09 13:02:20 -0600

    Initial release

---
+++ src/philiprehberger_result/__init__.py
@@ -0,0 +1,13 @@
+from .result import Ok, Err, Result, ok, err, try_catch, try_catch_async, from_awaitable, all_ok
+
+__all__ = [
+    "Ok",
+    "Err",
+    "Result",
+    "ok",
+    "err",
+    "try_catch",
+    "try_catch_async",
+    "from_awaitable",
+    "all_ok",
+]
